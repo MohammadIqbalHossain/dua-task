d5505a1ae135ed6fa76850c75b19238842254501
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2024-11-20 00:59:58 +0600

    project added

---
+++ database.js
@@ -0,0 +1,12 @@
+const sqlite3 = require('sqlite3');
+
+// Connect to SQLite database (replace 'your_database_file.sqlite' with your .sqlite file)
+const db = new sqlite3.Database('./db/dua_main.sqlite', (err) => {
+    if (err) {
+      console.error('Error connecting to SQLite database:', err.message);
+    } else {
+      console.log('Connected to the SQLite database.');
+    }
+  });
+  
+  module.exports = db
